9265591e76906330c1b296274f2ee8b1ff38f098
Konstantinos Skenteridis <kostaking@gmail.com>
2017-05-09 00:58:55 +0300

    Update srt-fixer.py

--- srt-fixer.py
+++ srt-fixer.py
@@ -1,8 +1,16 @@
 
+
 import sys
 import re
 import argparse
 
+# SKENTERIDIS KONSTANTINOS
+# A.M.: P2014221
+
+# -- Edw ginetai o entopismos kathe grammhs xronikhs plhroforias mesw kanonikhs ekfrashs
+# -- me morfh 00:00:00,000 --> 00:00:00,000
+restr = r'(\d{2}):(\d{2}):(\d{2}),(\d{3})\s-->\s(\d{2}):(\d{2}):(\d{2}),(\d{3})$'
+rexp = re.compile(restr)
 
 parser = argparse.ArgumentParser()
 # add mandatory (positional) arguments
@@ -12,12 +20,34 @@ parser.add_argument("offset",type=float,help="subtitle offset in seconds to appl
 # parse arguments
 args = parser.parse_args()
 
-with open(args.fname,newline='') as ifp:	
+with open(args.fname,'r+',newline='') as ifp:	
 	for line in ifp:
-	
-		# -- αντικαταστήστε με τον δικό σας κώδικα (αρχή) --
+		
+		
+		# -- Edw tha kanei anazhthsh se oloklhro to keimeno kai tha bgalei ta matches
+		m = rexp.search(line)
+		
+		# -- Parakatw ginetai h metatroph tou string twn deuteroleptwn se float,
+		# -- meta prostithetai to offset, kai sthn sunexeia metatrepetai pali pisw se string.
+		
+		if m is not None:
+			# -- Gia ta seconds sta aristera
+			left_seconds_old = m.group(3)
+			left_seconds_new = float(left_seconds_old)+args.offset
+			left_seconds_final = str(left_seconds_new)
+			changing_left = re.sub(restr,r'\3'+left_seconds_final,line)
+			
+			# -- Gia ta seconds sta deksia
+			right_seconds_old = m.group(7)
+			right_seconds_new = float(right_seconds_old)+args.offset
+			right_seconds_final = str(right_seconds_new)
+			changing_right = re.sub(restr,r'\7'+right_seconds_final,line)
+		
+		
 
 		sys.stdout.write(line)
 
-		# -- αντικαταστήστε με τον δικό σας κώδικα (τέλος) --
+		
+ifp.close()		
+		
 
